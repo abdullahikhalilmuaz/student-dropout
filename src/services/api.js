49b1e4a4c6d2e17df1160233504c8fd5dc3d8b92
abdullahikhalilmuaz <abdullahikhalilmuaz@gmail.com>
2026-08-18 19:21:49 +0100

    last frontend v2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://student-dropout-backend.onrender.com/api",
+  baseURL: "https://student-dropout-ml-server.onrender.com/api",
 });
 
 export default api;
