7a51e830c97a2b5179a59c990161b2dcbc4c17ab
abdullahikhalilmuaz <abdullahikhalilmuaz@gmail.com>
2026-08-18 18:53:43 +0100

    last

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://student-dropout-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -13,7 +13,7 @@ api.interceptors.response.use(
   (error) => {
     console.error("API Error:", error.response?.data || error.message);
     return Promise.reject(error);
-  }
+  },
 );
 
 export default api;
